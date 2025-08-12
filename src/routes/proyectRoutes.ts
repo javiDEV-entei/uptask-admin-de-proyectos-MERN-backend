4d01317523b38eeafa93ef5775d1b804ff428db5
javiDEV-entei <javieroliveradev0239@gmail.com>
2025-08-12 15:24:20 -0300

    validacion desde el servidor agregada
    se agrego validacion para el contenido de las request para la creacion de proyectos usando express-validator

--- src/routes/proyectRoutes.ts
+++ src/routes/proyectRoutes.ts
@@ -1,10 +1,21 @@
 import {Router} from 'express'
+import {body} from 'express-validator'
 import { ProjectController } from '../controllers/ProjectController'
+import { handleInputErrors } from '../middleware/validation'
 
 const router = Router()
 
 
-router.post('/',ProjectController.creeateProyect)
+router.post('/',
+    body('projectName')
+        .notEmpty().withMessage('El Nombre del Proyrcto es Obligatorio'),
+    body('clientName')
+        .notEmpty().withMessage('El Nombre del Cliente es Obligatorio'),
+    body('description')
+        .notEmpty().withMessage('La Descripcion del Proyrcto es Obligatoria'),
+    handleInputErrors,
+    ProjectController.creeateProyect)
+
 router.get('/',ProjectController.getAllProyects)
 
 export default router
