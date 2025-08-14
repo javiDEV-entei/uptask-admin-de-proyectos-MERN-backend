fe0ccfb37a455e8ab20e82cec33d9f10ee35b382
javiDEV-entei <javieroliveradev0239@gmail.com>
2025-08-14 15:51:19 -0300

    endpoints para actualizar y eliminar agregados
    se agregaron los endpoints y se crearon las funciones para eliminar y actualizar proyectos

--- src/controllers/ProjectController.ts
+++ src/controllers/ProjectController.ts
@@ -2,9 +2,10 @@ import type { Request, Response } from "express";
 import Project from "../models/Project";
 
 
+
 export class ProjectController {
 
-    static creeateProyect = async (req: Request, res: Response) =>{
+    static creeateProject = async (req: Request, res: Response) =>{
         
         const project = new Project(req.body)
 
@@ -22,7 +23,80 @@ export class ProjectController {
 
     static getAllProyects = async (req: Request, res: Response) =>{
         
-        res.send('Todos los proyectos')
+        try {
+            const projects = await Project.find({})
+            res.json(projects)
+        } catch (error) {
+            console.log(error);
+            
+        }
+        
+    }
+    static getProjectById = async (req: Request, res: Response) =>{
+        const {id} = req.params
+        
+        
+        try {
+            const project = await Project.findById(id)
+            if (!project) {
+                const error =new Error('Proyecto no encontrado')
+                return res.status(404).json({error:error.message})
+                
+            }
+
+            res.json(project)
+        } catch (error) {
+            console.log(error);
+            
+        }
+        
+    }
+
+
+    static updateProject = async (req: Request, res: Response) =>{
+        const {id} = req.params
+        
+        
+        try {
+            const project = await Project.findByIdAndUpdate(id, req.body)
+
+
+            if (!project) {
+                const error =new Error('Proyecto no encontrado')
+                return res.status(404).json({error:error.message})
+                
+            }
+            await project.save()
+
+            res.send('Proyecto actualizado')
+                 
+        } catch (error) {
+            console.log(error);
+            
+        }
+        
+    }
+    static deleteProject = async (req: Request, res: Response) =>{
+        const {id} = req.params
+
+        const project = await Project.findById(id)
+
+         if (!project) {
+                const error =new Error('Proyecto no encontrado')
+                return res.status(404).json({error:error.message})
+                
+            }
+
+        await project.deleteOne()
+        res.send('Proyecto eliminado')
+        
+        try {
+            
+                 
+        } catch (error) {
+            console.log(error);
+            
+        }
         
     }
 }
--- src/routes/proyectRoutes.ts
+++ src/routes/proyectRoutes.ts
@@ -1,5 +1,5 @@
 import {Router} from 'express'
-import {body} from 'express-validator'
+import {body, param} from 'express-validator'
 import { ProjectController } from '../controllers/ProjectController'
 import { handleInputErrors } from '../middleware/validation'
 
@@ -8,14 +8,35 @@ const router = Router()
 
 router.post('/',
     body('projectName')
-        .notEmpty().withMessage('El Nombre del Proyrcto es Obligatorio'),
+        .notEmpty().withMessage('El Nombre del Proyecto es Obligatorio'),
     body('clientName')
         .notEmpty().withMessage('El Nombre del Cliente es Obligatorio'),
     body('description')
-        .notEmpty().withMessage('La Descripcion del Proyrcto es Obligatoria'),
+        .notEmpty().withMessage('La Descripcion del Proyecto es Obligatoria'),
     handleInputErrors,
-    ProjectController.creeateProyect)
+    ProjectController.creeateProject)
 
 router.get('/',ProjectController.getAllProyects)
+
+router.get('/:id',
+    param('id').isMongoId().withMessage('id no valido'),
+    handleInputErrors,
+    ProjectController.getProjectById)
+
+router.put('/:id',
+    param('id').isMongoId().withMessage('id no valido'),
+    body('projectName')
+        .notEmpty().withMessage('El Nombre del Proyecto es Obligatorio'),
+    body('clientName')
+        .notEmpty().withMessage('El Nombre del Cliente es Obligatorio'),
+    body('description')
+        .notEmpty().withMessage('La Descripcion del Proyecto es Obligatoria'),
+    handleInputErrors,
+    ProjectController.updateProject)
+
+    router.delete('/:id',
+    param('id').isMongoId().withMessage('id no valido'),
+    handleInputErrors,
+    ProjectController.deleteProject)
 
 export default router
