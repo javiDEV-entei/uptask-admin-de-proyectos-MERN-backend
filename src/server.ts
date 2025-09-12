a0e14dfc4cc54db191540edd4b9080fbb23b5a05
javiDEV-entei <javieroliveradev0239@gmail.com>
2025-09-12 16:05:02 -0300

    dependencia morgan agregada

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
+import morgan from 'morgan'
 import { corsConfig } from './config/cors'
 import { conectDB } from './config/db'
 import projectRoutes from './routes/proyectRoutes'
@@ -13,6 +14,10 @@ conectDB()
 const app = express()
 app.use(cors(corsConfig))
 
+//Logging
+app.use(morgan('dev'))
+
+//leer datos delos formularios
 app.use(express.json())
 
 //Routes
