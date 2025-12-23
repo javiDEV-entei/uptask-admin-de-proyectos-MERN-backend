af28ce49cd1e90652ff9436617b465675ec33056
javiDEV-entei <javieroliveradev0239@gmail.com>
2025-12-23 16:55:51 -0300

    funciones y rutas para actualizar perfil y password add
    se agregaron al backend los endpoints y las funciones para actualizar el perfil y la contraseña del usuario

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -244,4 +244,50 @@ export class AuthController {
       return res.json(req.user)
   }
 
+  static updateProfile = async (req: Request, res: Response) => {
+
+    const {name, email} = req.body
+
+    const userExist = await User.findOne({email})
+    if (userExist && userExist.id.toString() !== req.user.id.toString() ) {
+      const error = new Error('Este email ya esta registrado')
+      return res.status(409).json({error: error.message})
+      
+    }
+
+    req.user.name = name
+    req.user.email = email
+
+    try {
+      await req.user.save()
+      res.send('Perfil actualizado correctamente') 
+    } catch (error) {
+        res.status(500).send('hubo un error')
+      
+    }
+  }
+
+    static updateCurrentUserPassword = async (req: Request, res: Response) => {
+
+      const {current_password, password} = req.body
+      const user = await User.findById(req.user.id)
+
+
+      const isPasswordCorrect = await checkPassword(current_password, user.password)
+      if (!isPasswordCorrect) {
+        const error = new Error('El password actual es incorrecto')
+      return res.status(401).json({error: error.message})
+        
+      }
+
+      try {
+        user.password = await hashPassword(password)
+        await user.save()
+        res.send('El password se modifico correctamente')
+      } catch (error) {
+        res.status(500).send('hubo un error')
+      }
+
+    }
+
 }
--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -8,87 +8,118 @@ const router = Router()
 
 router.post('/create-account',
     body('name')
-        .notEmpty().withMessage('el nombre de usuario no puede ir vacio'), 
+        .notEmpty().withMessage('el nombre de usuario no puede ir vacio'),
     body('password')
-        .isLength({min: 8}).withMessage('El password es muy corto, minimo 8 caracteres'),
-    body('password_confirmation').custom((value, {req} )=>{
-        if (value!== req.body.password) {
+        .isLength({ min: 8 }).withMessage('El password es muy corto, minimo 8 caracteres'),
+    body('password_confirmation').custom((value, { req }) => {
+        if (value !== req.body.password) {
             throw new Error('Los password no son iguales')
-            
+
         }
         return true
-        
-    }), 
+
+    }),
     body('email')
         .isEmail().withMessage('E-mail no valido'),
-    handleInputErrors, 
-    
+    handleInputErrors,
+
     AuthController.createAccount
 )
 
-router.post('/confirm-account', 
+router.post('/confirm-account',
     body('token')
         .notEmpty().withMessage('El token no puede ir vacio'),
-        handleInputErrors,
-        AuthController.confirmAccount
+    handleInputErrors,
+    AuthController.confirmAccount
 )
 
 
-router.post('/login', 
+router.post('/login',
     body('email')
         .isEmail().withMessage('E-mail no valido'),
     body('password')
         .notEmpty().withMessage('El password no puede ir vacio'),
-        handleInputErrors,
-        AuthController.login
+    handleInputErrors,
+    AuthController.login
 )
 
 
 
-router.post('/request-code', 
+router.post('/request-code',
     body('email')
         .isEmail().withMessage('E-mail no valido'),
-        handleInputErrors,
-        AuthController.requestConfirmationCode
+    handleInputErrors,
+    AuthController.requestConfirmationCode
 )
 
 
 
-router.post('/forgot-password', 
+router.post('/forgot-password',
     body('email')
         .isEmail().withMessage('E-mail no valido'),
-        handleInputErrors,
-        AuthController.forgotPassword
+    handleInputErrors,
+    AuthController.forgotPassword
 )
 
 router.post('/validate-token',
     body('token')
         .notEmpty().withMessage('El token no puede ir vacio'),
-        handleInputErrors,
-        AuthController.validateToken
+    handleInputErrors,
+    AuthController.validateToken
 )
 
 
 
 router.post('/update-password/:token',
-     param('token').isNumeric().withMessage('Token no valido'),
-     body('password')
-        .isLength({min: 8}).withMessage('El password es muy corto, minimo 8 caracteres'),
-    body('password_confirmation').custom((value, {req} )=>{
-        if (value!== req.body.password) {
+    param('token').isNumeric().withMessage('Token no valido'),
+    body('password')
+        .isLength({ min: 8 }).withMessage('El password es muy corto, minimo 8 caracteres'),
+    body('password_confirmation').custom((value, { req }) => {
+        if (value !== req.body.password) {
             throw new Error('Los password no son iguales')
-            
+
         }
         return true
-        
-    }), 
-        handleInputErrors,
-        AuthController.updatePasswordWithToken
+
+    }),
+    handleInputErrors,
+    AuthController.updatePasswordWithToken
 )
 
 router.get('/user',
     authenticate,
     AuthController.user
+)
+
+
+/** Profile */
+
+router.put('/profile',
+    authenticate,
+   body('name')
+        .notEmpty().withMessage('El nombre no puede ir vacio'),
+    body('email')
+        .isEmail().withMessage('E-mail no valido'),
+    handleInputErrors,
+    AuthController.updateProfile
+)
+
+router.post('/update-password',
+    authenticate,
+    body('current_password')
+        .notEmpty().withMessage('El password actual no puede ir vacio'),
+     body('password')
+        .isLength({ min: 8 }).withMessage('El password es muy corto, minimo 8 caracteres'),
+    body('password_confirmation').custom((value, { req }) => {
+        if (value !== req.body.password) {
+            throw new Error('Los password no son iguales')
+
+        }
+        return true
+
+    }),
+    handleInputErrors,
+    AuthController.updateCurrentUserPassword
 )
 
 
